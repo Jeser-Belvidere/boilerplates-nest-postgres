a9d03421990bbb9be4cdb3830b1c4dee614d0849
Ivan Petrovskiy <jelbelvidere@gmail.com>
2023-01-06 13:37:55 +0300

    Added: database connection in app module and docker-compose.yml

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,30 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+import { TerminusModule } from '@nestjs/terminus';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from '../users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TerminusModule,
+    HttpModule,
     UsersModule,
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: 5432,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true, // models will be loaded automatically
+      synchronize: !process.env.PRODUCTION_MODE || false,
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
