42ed03644f47e294d0a37280010bd72f50e5844d
Ivan Petrovskiy <jelbelvidere@gmail.com>
2023-02-19 16:28:36 +0300

    Added swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,16 @@ async function bootstrap() {
       },
     }),
   );
+
+  const options = new DocumentBuilder()
+    .setTitle('Nest Template')
+    .setDescription('Nest template application')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(parseInt(process.env.APP_PORT || '3000', 10), () => {
     console.log(`APP HAS BEEN STARTED ON PORT ${process.env.APP_PORT}`);
   });
